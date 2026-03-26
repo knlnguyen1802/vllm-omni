fcd91a0b67869acf9f30b99e713b38717bde8d34
knlnguyen1802 <knlnguyen1802@gmail.com>
2026-03-26 14:22:43 +0800

    Fix pre-commit
    Signed-off-by: knlnguyen1802 <knlnguyen1802@gmail.com>

--- tests/e2e/offline_inference/custom_pipeline/test_worker_extension.py
+++ tests/e2e/offline_inference/custom_pipeline/test_worker_extension.py
@@ -11,8 +11,8 @@ from tests.e2e.offline_inference.custom_pipeline.worker_extension import (
     vLLMOmniColocateWorkerExtensionForTest,
 )
 from tests.utils import hardware_test
-from vllm_omni.entrypoints.async_omni import AsyncOmni
 from vllm_omni.diffusion.worker.diffusion_worker import CustomPipelineWorkerExtension
+from vllm_omni.entrypoints.async_omni import AsyncOmni
 
 CUSTOM_PIPELINE_CLASS = (
     "tests.e2e.offline_inference.custom_pipeline.qwen_image_pipeline_with_logprob.QwenImagePipelineWithLogProbForTest"
